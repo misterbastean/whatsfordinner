308d2a31e11519ba4979997cb3b72c56a8fb4807
Josh Bastean <misterbastean@gmail.com>
2020-10-06 09:57:08 -0400

    Add confirmation popup prior to deleting

--- public/js/account.js
+++ public/js/account.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     $(this).closest('form').attr('action', '/favorites_calendar');
   });
   $("#deleteButton").click(function() {
-    $(this).closest('form').attr('action', '/favorites_delete');
+    const conf = confirm("Are you sure you want to delete these meals? This CANNOT be undone!")
+    if (conf) {
+      $(this).closest('form').attr('action', '/favorites_delete').unbind("submit");
+    } else {
+      $(this).closest('form').submit(function (e) {
+        e.preventDefault();
+      })
+    }
   });
 });
